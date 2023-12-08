d147c70999c72ccc6269c116d8af185f313a91d0
smcdonou02 <smcdonou@stevens.edu>
2023-12-08 01:30:30 -0500

    Part2 files complete

--- VS/src/part2/ComputerPartVisitor.java
+++ VS/src/part2/ComputerPartVisitor.java
@@ -4,4 +4,10 @@ public interface ComputerPartVisitor {
 
 	public void visit(Keyboard keyboard);
 
+	public void visit(Monitor monitor);
+
+	public void visit(Mouse mouse);
+
+	public void visit(Computer computer);
+
 }
